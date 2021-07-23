6575362988b7b904c654811cf6fe2d037b152bc5
marcos <marcos@grana.capital>
2021-07-23 20:20:01 -0300

    changed arguments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const hostUrl = process.env.HOST_URL
-const startingPort = process.env.STARTING_PORT
-const endingPort = process.env.ENDING_PORT
+const hostUrl = process.argv[2]
+const startingPort = process.argv[3]
+const endingPort = process.argv[4]
 
 console.log(hostUrl, startingPort, endingPort)
